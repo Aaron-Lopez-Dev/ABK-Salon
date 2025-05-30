6c17073abc44e6670c669203d333821c16c4b62b
Aaron Lopez <95320154+Aaron-Lopez-Dev@users.noreply.github.com>
2025-05-29 23:03:46 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 window.addEventListener('scroll', function() {
+
     const navbar = document.getElementById('navbar');
+    const name = document.getElementById('name');
+    const links = document.getElementById('links');
+    const links2 = document.getElementById('links2');
+
     if (window.scrollY > 200) {
       navbar.classList.add('scrolled');
+      name.classList.add('scrolled');
+      links.classList.add('scrolled');
+      links2.classList.add('scrolled');
 
     } else {
       navbar.classList.remove('scrolled');
+      name.classList.remove('scrolled');
+      links.classList.remove('scrolled');
+      links2.classList.remove('scrolled');
     }
   });
+  
+
   
