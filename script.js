0ff25aa16f5167b81db94c29bcebae93238324ba
Aaron Lopez <95320154+Aaron-Lopez-Dev@users.noreply.github.com>
2025-07-06 18:53:20 -0400

    .

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', monitorYPosition)
+window.addEventListener('scroll', navBarColorChange)
 
-    const navbar = document.getElementById('navbar');
-    const name = document.getElementById('name');
-    const links = document.getElementById('links');
-    const links2 = document.getElementById('links2');
+const heroSection = document.getElementById('hero')
+const serviceSection = document.getElementById('services')
+const contactSection = document.getElementById('contact')
+const sectionTitle = document.getElementById('sectionName')
+const navbar = document.getElementById('navbar');
+const sectionName = document.getElementById('sectionName');
 
-    if (window.scrollY > 200) {
-      navbar.classList.add('scrolled');
-      name.classList.add('scrolled');
-      links.classList.add('scrolled');
-      links2.classList.add('scrolled');
 
-    } else {
-      navbar.classList.remove('scrolled');
-      name.classList.remove('scrolled');
-      links.classList.remove('scrolled');
-      links2.classList.remove('scrolled');
-    }
-  });
-  
+function navBarColorChange() {
 
-  
+  if (window.scrollY > 200) {
+    navbar.classList.add('scrolled');
+    sectionName.classList.add('scrolled');
+
+
+  } else {
+    navbar.classList.remove('scrolled');
+    sectionName.classList.remove('scrolled');
+  }
+};
+
+function monitorYPosition() {
+
+  const heroSectionPosition = heroSection.getBoundingClientRect()
+  const serviceSectionPosition = serviceSection.getBoundingClientRect()
+  const contactSectionPosition = contactSection.getBoundingClientRect()
+
+  const yTarget = window.innerHeight / 2
+  console.log(heroSectionPosition.y + " and " + serviceSectionPosition.y)
+  if (heroSectionPosition.y == 0 && serviceSectionPosition.y > yTarget * .80) {
+    sectionTitle.textContent = "ABK SALON"
+    console.log("YES")
+  }
+  else if (serviceSectionPosition.y <= yTarget && serviceSectionPosition.y >= 0 && contactSectionPosition.y > yTarget * .80) {
+    sectionTitle.textContent = 'SERVICES'
+  }
+  else if (contactSectionPosition.y <= yTarget && contactSectionPosition.y >= 0) {
+    sectionTitle.textContent = 'CONTACT'
+  }
+
+}
+// For Initial Load
+monitorYPosition()
